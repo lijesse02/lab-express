9bc1b26a902afd918be473ac24448cb95acb7180
lijesse <lijesse@umich.edu>
2025-01-22 13:15:57 -0500

    made 1/2 changes for prod

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,7 @@ from flask_cors import CORS
 from init import initialize_data, redis_client
 import os
 import json
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -105,6 +106,70 @@ def decode_word():
     return jsonify({"barcodes": [order_info]})
 
 
+@app.route('/api/decode-excel', methods=['POST'])
+def decode_excel():
+    #receive data
+    if 'file' not in request.files:
+        return jsonify({"error": "No Excel File Provided"}), 400
+    
+    excel_file = request.files['file']
+
+    try:
+        redis_client.delete("barcode_to_items")
+        df = pd.read_excel(excel_file)
+        current_order_number = ""
+        items_in_order = []
+        for row in df.itertuples(index=True):
+            #check if the row is an order item
+            if pd.isna(row.Num):
+                #check if the order has been stored yet
+                if items_in_order:
+                    #store order and reset
+                    redis_client.hset("barcode_to_items", current_order_number, json.dumps(items_in_order))
+                    current_order_number = ""
+                    items_in_order = []
+            else:
+                #check if row is a new order
+                if current_order_number == "":
+                    current_order_number = row.Num[1:-1]
+                    items_in_order = [[row.Item, row.Qty]]
+                #check if order number is a different order number
+                elif current_order_number != row.Num[1:-1]:
+                    #store if different order number
+                    redis_client.hset("barcode_to_items", current_order_number, json.dumps(items_in_order))
+                    current_order_number = row.Num[1:-1]
+                    items_in_order = [[row.Item, row.Qty]]
+                #check if item is shipping and handling
+                elif "shipping and handling" in row.Item:
+                    continue
+                #another item in the order
+                else:
+                    #add item to order
+                    items_in_order.append([row.Item, row.Qty])
+        return jsonify({"status": "success! data imported"}), 200
+    except Exception as e:
+        error_type = type(e).__name__
+        error_message = str(e)
+        return jsonify({
+            "status": "failed",
+            "error_type": error_type,
+            "error_message": error_message
+        }), 500
+    return jsonify({"status": "success! data imported"})
+
+@app.route('/api/get-order-info', methods=['POST'])
+def getOrderInfo():
+    #grab data from request
+    data = request.get_json()
+    items = redis_client.hget('barcode_to_items', data["barcode"])
+    if items:
+        items = json.loads(items)
+        itemList = []
+        for item in items:
+            itemList.append({"name": item[0], "quantity": item[1]})
+        return jsonify({"status": "success", "items": itemList})
+    return jsonify({'status': "failure. No items in order"})
+
 @app.route('/api/get-item-info', methods=['POST', 'OPTIONS'])
 def getItemInfo():
     if request.method == 'OPTIONS':
@@ -122,6 +187,8 @@ def getItemInfo():
         itemData = json.loads(itemData)
         itemName = itemData["itemName"]
         itemSize = itemData["itemSize"]
+        itemUM = itemData["itemUM"]
+        itemQuantity = int(itemData["itemQuantity"])
 
         #Make list of item sizes
         sizeList = {
@@ -133,8 +200,8 @@ def getItemInfo():
             "btp": 0,
         }
         for item in data["items"]:
-            sizeList[item.get("item_size")] += item.get("quantity")
-        sizeList[itemData["itemSize"]] += 1
+            sizeList[item.get("item_size")] += int(item.get("item_quantity"))
+        sizeList[itemData["itemSize"]] += int(itemData["itemQuantity"])
 
         #return the box(es) to use as a string
         boxes = logicDict(sizeList)
@@ -143,6 +210,8 @@ def getItemInfo():
                         "status": "success!",
                         "itemName": itemName,
                         "itemSize": itemSize,
+                        "itemUM": itemUM,
+                        "itemQuantity":
                         "sizeList": sizeList,
                         "boxes": boxes
                         })
